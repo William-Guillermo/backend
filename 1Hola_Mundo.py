6b5ec6cbd59cd1afe419d082aa660304849df08e
William-Guillermo <guillermodelapazw@gmail.com>
2022-01-14 20:52:42 -0600

    Modificado error de escritura

--- 1Hola_Mundo.py
+++ 1Hola_Mundo.py
@@ -1,4 +1,4 @@
 #  primer programa en python utlizando Git en un equipo Linux Lite  
 
-print("Binvenido al maravilloso mundo de linux Lite y Python no esperan grandes aventuras")
+print("Bienvenido al maravilloso mundo de linux Lite y Python nos esperan grandes aventuras")
 print("Git Init que grande es Python")

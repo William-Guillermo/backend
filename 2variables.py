1d43de86e330a59be9fc1ed0297f6b08683246de
William-Guillermo <guillermodelapazw@gmail.com>
2022-01-14 23:17:31 -0600

    datos desde el imput

--- 2variables.py
+++ 2variables.py
@@ -12,4 +12,9 @@ print(edad)
 print(fecha_nacimiento)
 print(masculino)
 
+ingresa_dato = str(input("Ingresa tu nombre:"))
+ingresa_edad = str(input("Ingresa tu edad:"))
+print(f"hola señor: {ingresa_dato}, su edad es: {ingresa_edad}")
+print("Saludos desde imput")
+
 (" saludos contunuamos practicando para el perfil de Baakc en Python")

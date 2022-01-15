fe9f16f86303ed96576dd3ac954051bec0ea7bb6
William-Guillermo <guillermodelapazw@gmail.com>
2022-01-14 22:49:56 -0600

    se agrego una programa mas  ejemplos de variables

---
+++ 2variables.py
@@ -0,0 +1,15 @@
+# variables en Python
+
+# cadena de caracteres String
+nombre = "William Guillermo"
+edad = 37
+fecha_nacimiento = 1984
+masculino = True
+
+print("Datos introducidos")
+print(nombre)
+print(edad)
+print(fecha_nacimiento)
+print(masculino)
+
+(" saludos contunuamos practicando para el perfil de Baakc en Python")

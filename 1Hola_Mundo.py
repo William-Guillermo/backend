222102ba9218b6b9812f6ab8b5a0ea37bd289c60
William-Guillermo <guillermodelapazw@gmail.com>
2022-01-14 19:48:12 -0600

    Mi primer Commit con Git y Linux Lite

---
+++ 1Hola_Mundo.py
@@ -0,0 +1,4 @@
+#  primer programa en python utlizando Git en un equipo Linux Lite  
+
+print("Binvenido al maravilloso mundo de linux Lite y Python no esperan grandes aventuras")
+print("Git Init que grande es Python")

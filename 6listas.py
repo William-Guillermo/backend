57d6893bbc6b21cfa462393815f19819499e8d39
William-Guillermo <guillermodelapazw@gmail.com>
2022-01-27 22:45:04 -0600

    Listas en Python

---
+++ 6listas.py
@@ -0,0 +1,15 @@
+# Scrip de listas en python
+
+nombre = ["william","Guillermo","Alonso"]
+print(nombre)
+nombre.append("Camila") # para agregar un elemnto a la lista al final
+print(nombre)
+nombre.pop() # elimina el ultimo elemento de una lista
+print(nombre)
+numero_elemntos = len(nombre)
+
+edades = [25, 30, 45, 18,52]
+print(f"los numeros de la lista son: {edades}")
+print(f"el numero maximo de la lista es:{max(edades)}")
+print(f"el numero minimo de la lista es:{min(edades)}")
+print(len(edades))

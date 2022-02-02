d3040d4c60e2051ed4760a6d16ce4bc8d30227d3
William-Guillermo <guillermodelapazw@gmail.com>
2022-02-01 18:51:58 -0600

    Diccionarios

---
+++ 7diccionarios.py
@@ -0,0 +1,14 @@
+
+# Scrip de Python para el tema de los dicionarios, los diccionarios tambien se pueden ocupart
+# como formatos Json para el envio de informacion del backend al front y viseversa
+contactos =  {
+    'id': 2,
+    'nombre' :'William',
+    'apellido' : 'Guillermo',
+    'edad' :38
+}
+
+print(contactos)
+
+for contacto in contactos:
+    print(contactos[contacto])

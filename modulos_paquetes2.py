6be90cb094f44406d3c9595ff2a4522d80a4ef15
William-Guillermo <guillermodelapazw@gmail.com>
2022-02-02 18:40:48 -0600

    Paquetes y modulos  con comentarios

--- modulos_paquetes2.py
+++ modulos_paquetes2.py
@@ -1,3 +1,13 @@
-import modulos_paquetes1
+#impoprta  todos los modulos, variables, funciones
+import modulos_paquetes1     
+"""si solo se requiere una funcion en especial solo se manda a llamar dicha funcio
+ se declara 
+ 
+ from  modulos_paquetes1 import Usuario
+ 
+ """
 
 print(modulos_paquetes1.saludar('William Guillermo'))
+user = modulos_paquetes1.Usuario('William','guillermodelapazw@gmail.com')
+
+print(user.nombre)

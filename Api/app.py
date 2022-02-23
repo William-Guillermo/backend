ddadc4ce6da09d920c623503ccb24ea4e61876fc
William-Guillermo <guillermodelapazw@gmail.com>
2022-02-22 18:43:17 -0600

    Levantando el servicio de una API

---
+++ Api/app.py
@@ -0,0 +1,12 @@
+
+from distutils.log import debug
+from main import create_app
+import os
+
+app = create_app()
+
+
+app.app_context().push()
+
+if __name__=='__main__':
+    app.run(port=os.getenv("PORT"),debug=True)
